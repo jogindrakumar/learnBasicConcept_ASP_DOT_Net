b43ea6cc94f7d42ad8a9403cf66bcac0dab38988
jogindrakumar <joginder9868@outlook.com>
2023-07-23 17:03:56 +0530

    Button Controls

---
+++ Button_LinkButton_and_ImageButton_controls.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Button_LinkButton_and_ImageButton_controls.aspx.cs" Inherits="learnBasicConcept_ASP_DOT_Net.Button_LinkButton_and_ImageButton_controls" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Button_LinkButton_and_ImageButton_controls.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace learnBasicConcept_ASP_DOT_Net
+{
+    public partial class Button_LinkButton_and_ImageButton_controls : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // or you can add event handler on button on page load like
+            Button1.Click += new EventHandler(Button1_Click);
+            //Button1.Command +=  CommandEventHandler(Button1_Command);
+           
+        }
+
+        private void Button1_Command1(object sender, CommandEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Command(object sender, CommandEventArgs e)
+        {
+
+        }
+    }
+}
